83d82b08a6f372f2516d19517e9a82884aaa26b5
Jay Feather2000 <thegals4321@gmail.com>
2023-01-27 15:49:18 -0600

    idk, mouse stuff

--- OpenGL Window Helper/src/Graphics/Mouse/Mouse.cpp
+++ OpenGL Window Helper/src/Graphics/Mouse/Mouse.cpp
@@ -10,9 +10,13 @@ namespace jf
     glm::vec2 Mouse::getPosition()
     {
         GLFWwindow* window = glfwGetCurrentContext();
-        glm::vec<2, double, glm::packed_highp> pos;
-        glfwGetCursorPos(window, &pos.x, &pos.y);
-        return glm::vec2(pos);
+        glm::vec<2, double, glm::packed_highp> mousePos;
+        glfwGetCursorPos(window, &mousePos.x, &mousePos.y);
+        
+        glm::vec<2, int, glm::packed_highp> windowPos;
+        glfwGetWindowPos(window, &windowPos.x, &windowPos.y);
+
+        return glm::vec2(mousePos) + glm::vec2(windowPos);
     }
     glm::vec2 Mouse::getPosition(GLFWwindow* window)
     {
